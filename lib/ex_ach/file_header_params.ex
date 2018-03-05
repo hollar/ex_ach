9f6c418570bb236fb1b0f6eca331eead1f36c5d4
Nicolas Charlery <nicolas@bokay.io>
2018-03-05 16:21:44 -0500

    Add missing file header validation

--- lib/ex_ach/file_header_params.ex
+++ lib/ex_ach/file_header_params.ex
@@ -14,6 +14,7 @@ defmodule ExAch.FileHeaderParams do
   ]
 
   validates(:immediate_destination, presence: true, format: ~r/^[[:alnum:]]+$/)
+  validates(:immediate_destination_name, length: [max: 23], format: ~r/^[[:alnum:] ]+$/)
 
   validates(
     :immediate_origin,
@@ -22,11 +23,11 @@ defmodule ExAch.FileHeaderParams do
     format: ~r/[0-9]{10}/
   )
 
+  validates(:immediate_origin_name, length: [max: 23], format: ~r/^[[:alnum:] ]+$/)
   validates(:file_creation_date, presence: true, length: 6, format: ~r/^[[:digit:]]+$/)
   validates(:file_creation_time, length: 4, format: ~r/^[[:digit:]]+$/)
   validates(:file_id_modifier, presence: true, format: ~r/^[[:alnum:]]+$/)
   validates(:reference_code, presence: true, format: ~r/^[[:alnum:]]+$/)
-  validates(:immediate_origin_name, length: [max: 23], format: ~r/^[[:alnum:] ]+$/)
 
   @spec new(ExAch.file_header_params()) :: %__MODULE__{}
   def new(params) do
@@ -34,11 +35,11 @@ defmodule ExAch.FileHeaderParams do
       immediate_destination: params.immediate_destination,
       immediate_destination_name: params[:immediate_destination_name],
       immediate_origin: params.immediate_origin,
-      immediate_origin_name: params.immediate_origin_name,
+      immediate_origin_name: params[:immediate_origin_name],
       file_creation_date: params.file_creation_date,
       file_creation_time: params[:file_creation_time],
       file_id_modifier: params.file_id_modifier,
-      reference_code: params.reference_code
+      reference_code: params[:reference_code]
     }
   end
 end
